b46dd253b1e87aeda6fe941c0bd5d02e756a128b
KaustubhPatange <developerkp16@gmail.com>
2026-01-25 14:06:54 +0530

    Update build script for rp2350 one

--- build_pico_fido.sh
+++ build_pico_fido.sh
@@ -10,16 +10,20 @@ SUFFIX="${VERSION_MAJOR}.${VERSION_MINOR}"
 mkdir -p build_release
 mkdir -p release
 rm -rf -- release/*
-cd build_release
 
-PICO_SDK_PATH="${PICO_SDK_PATH:-../../pico-sdk}"
+# Get absolute paths before changing directory
+PICO_SDK_PATH="/opt/pico-sdk"
 SECURE_BOOT_PKEY="${SECURE_BOOT_PKEY:-../../ec_private_key.pem}"
-boards=("pico" "pico2")
+PICO_PLATFORM="rp2350"
+PICO_SDK_TOOLCHAIN="/opt/gcc-arm-none-eabi"
+
+cd build_release
+boards=("waveshare_rp2350_one")
 
 for board_name in "${boards[@]}"
 do
     rm -rf -- ./*
-    PICO_SDK_PATH="${PICO_SDK_PATH}" cmake .. -DPICO_BOARD=$board_name -DSECURE_BOOT_PKEY=${SECURE_BOOT_PKEY}
-    make -j`nproc`
+    PICO_SDK_PATH="${PICO_SDK_PATH}" PICO_TOOLCHAIN_PATH="${PICO_SDK_TOOLCHAIN}" PICO_PLATFORM="${PICO_PLATFORM}" cmake .. -DPICO_BOARD=$board_name -DENABLE_EDDSA=1
+    make -j4
     mv pico_fido.uf2 ../release/pico_fido_$board_name-$SUFFIX.uf2
 done
