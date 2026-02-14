13a056893261876940a07419f96e2ebbd002edb7
phoeagon <phoeagon@gmail.com>
2026-02-13 22:29:04 -0800

    revert changes back to https://github.com/polhenarejos/pico-fido.git

--- build_pico_fido.sh
+++ build_pico_fido.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION_MAJOR="7"
-VERSION_MINOR="2"
+VERSION_MINOR="4"
 SUFFIX="${VERSION_MAJOR}.${VERSION_MINOR}"
 #if ! [[ -z "${GITHUB_SHA}" ]]; then
 #    SUFFIX="${SUFFIX}.${GITHUB_SHA}"
@@ -10,26 +10,16 @@ SUFFIX="${VERSION_MAJOR}.${VERSION_MINOR}"
 mkdir -p build_release
 mkdir -p release
 rm -rf -- release/*
-
-# Get absolute paths before changing directory
-PICO_SDK_PATH="/opt/pico-sdk"
-SECURE_BOOT_PKEY="${SECURE_BOOT_PKEY:-../../ec_private_key.pem}"
-PICO_PLATFORM="rp2350"
-PICO_SDK_TOOLCHAIN="/opt/gcc-arm-none-eabi"
-
 cd build_release
-boards=("waveshare_rp2350_one") # See all_boards.txt
+
+PICO_SDK_PATH="${PICO_SDK_PATH:-../../pico-sdk}"
+SECURE_BOOT_PKEY="${SECURE_BOOT_PKEY:-../../ec_private_key.pem}"
+boards=("pico" "pico2")
 
 for board_name in "${boards[@]}"
 do
     rm -rf -- ./*
-    PICO_SDK_PATH="${PICO_SDK_PATH}" PICO_TOOLCHAIN_PATH="${PICO_SDK_TOOLCHAIN}" PICO_PLATFORM="${PICO_PLATFORM}" cmake .. \
-        -DPICO_BOARD=$board_name \
-        -DENABLE_EDDSA=1 \
-        -DENABLE_POWER_ON_RESET=1 \ # if you want to support Reset functionality
-        -DENABLE_OATH_APP=1 \
-        -DENABLE_OTP_APP=1 \
-        -DVIDPID=Yubikey5
-    make -j4
+    PICO_SDK_PATH="${PICO_SDK_PATH}" cmake .. -DPICO_BOARD=$board_name -DSECURE_BOOT_PKEY=${SECURE_BOOT_PKEY}
+    make -j`nproc`
     mv pico_fido.uf2 ../release/pico_fido_$board_name-$SUFFIX.uf2
 done
